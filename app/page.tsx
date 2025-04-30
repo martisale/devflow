72d64b5c4d307c3754dc1eb1a2633374e0d5a74d
Martin Salewski <martinsalewski2000@gmail.com>
2025-05-01 00:07:24 +0200

    EsLint, Prettier and Tailwind configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-
 export default function Home() {
-  return (
-    <h1 className="text-4xl font-bold text-violet-700">Welcome to DevFlow</h1>
-  );
+  return <h1 className="font-bold text-red text-4xl">Welcome to DevFlow</h1>;
 }
