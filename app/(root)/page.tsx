ca7b49455ac8d4acc33e1c260307506ce4cdfbbc
Martin Salewski <martinsalewski2000@gmail.com>
2025-05-02 23:53:05 +0200

    Mobile NavBar and NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -12,16 +10,6 @@ const Home = async () => {
       <h1 className="font-bold text-4xl font-space-grotesk">
         Welcome to DevFlow
       </h1>
-      <form
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-        className="px-10 pt-[100px]"
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 };
