ea54dc962085aff07907e349e3c9d95b37eaa6bb
Martin Salewski <martinsalewski2000@gmail.com>
2025-05-05 19:34:46 +0200

    Implement Tag Input

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { Badge } from "../ui/badge";
 import { getDeviconClassName } from "@/lib/utils";
+import Image from "next/image";
 
 interface Props {
   _id: string;
@@ -10,27 +11,57 @@ interface Props {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+const TagCard = ({
+  _id,
+  name,
+  questions,
+  showCount,
+  compact,
+  remove,
+  isButton,
+  handleRemove,
+}: Props) => {
   const iconClass = getDeviconClassName(name);
 
-  return (
-    <div>
-      <Link className="flex justify-between gap-2" href={ROUTES.TAGS(_id)}>
-        <Badge className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
-          <div className="flex-center space-x-2">
-            <i className={`${iconClass} text-sm`}></i>
-            <span>{name}</span>
-          </div>
-        </Badge>
+  const content = (
+    <>
+      <Badge className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+        <div className="flex-center space-x-2 flex flex-row gap-2">
+          <i className={`${iconClass} text-sm`}></i>
+          <span>{name}</span>
+        </div>
 
-        {showCount && (
-          <p className="small-mdeium text-dark500_light700">{questions}</p>
+        {remove && (
+          <Image
+            src="/icons/close.svg"
+            alt="delete"
+            width={12}
+            height={12}
+            className="cursor-pointer object-contain invert-0 dark:invert"
+            onClick={handleRemove}
+          />
         )}
-      </Link>
-    </div>
+      </Badge>
+      {showCount && (
+        <p className="small-mdeium text-dark500_light700">{questions}</p>
+      )}
+    </>
   );
+
+  if (compact) {
+    return isButton ? (
+      <button className="flex justify-between gap-2">{content}</button>
+    ) : (
+      <Link className="flex justify-between gap-2" href={ROUTES.TAGS(_id)}>
+        {content}
+      </Link>
+    );
+  }
 };
 
 export default TagCard;
